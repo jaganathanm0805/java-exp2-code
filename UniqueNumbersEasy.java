000553ff40e9309328ddb069395791acc55208bf
jaganathanm0805 <jaganathanm@karunya.edu.in>
2025-09-05 12:22:37 +0530

    Update UniqueNumbersEasy.java

--- UniqueNumbersEasy.java
+++ UniqueNumbersEasy.java
@@ -1,22 +1,16 @@
 package exp2;
 import java.util.Scanner;
-
 public class UniqueNumbersEasy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Input
         int n;
         System.out.print("Enter total numbers: ");
         n = sc.nextInt();
         int arr[] = new int[n];
-
         System.out.println("Enter numbers: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        // Find unique numbers
         int count = 0;
         System.out.print("Unique numbers are: ");
         for (int i = 0; i < n; i++) {
@@ -36,3 +30,4 @@ public class UniqueNumbersEasy {
         sc.close();
     }
 }
+
